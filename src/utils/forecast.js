a4aa93b2aa9201f3367a6dcfcecf91bbf0b8d743
Idnani <riidnani@deloitte.com>
2022-09-14 15:03:27 -0400

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (longitude, latitude, callback) => {
+    url = 'http://api.weatherstack.com/current?access_key=cb15331c5e4f2d748c6307f80658a36a&query='+ latitude + ',' + longitude +'&units=f'
+
+    request({url, json: true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect Weatherstack.', undefined)
+        } else if (body.error) {
+            callback('Please check your coordinates.', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out.')
+        }
+    })
+}
+
+module.exports = forecast
